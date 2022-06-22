9888cec3fb82ce8e5fbab3d1252ea6ce6e20b8ff
MercyChebet <chebetm96@gmail.com>
2022-06-22 08:45:48 -0700

    One

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,17 +2,17 @@
 /**
  * _puts_recursion - prints a string in reverse
  * s: pointer block of memory to fill
- * *Return: void
+ * 
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
-void _puts_recursion(char *s);
+void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
-
 	_putchar (*s);
 	_puts_recursion(s + 1);
 
