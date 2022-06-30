005d1f7f23465ec9e81ed7bd85fc2c55552a435d
MercyChebet <chebetm96@gmial.com>
2022-06-30 04:40:52 -0700

    One

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -24,7 +24,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		}
 		for (; j < size_2; j++)
 		{
-			ew_string[i + j] = s2[j];
+			new_string[i + j] = s2[j];
 		}
 		new_string[i + j] = '\0';
 	}
