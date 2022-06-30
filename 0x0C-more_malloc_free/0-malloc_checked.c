9d863e4b6e53674e45867e34808f2275bbce5cf0
MercyChebet <chebetm96@gmial.com>
2022-06-30 11:11:36 -0700

    One

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,41 +2,19 @@
 #include <stdlib.h>
 
 /**
- * string_nconcat - Concatenates two strings using at
- * most an inputted number of bytes.
- * @s1: The first string.
- * @s2: The second string.
- * @n: The maximum number of bytes of s2 to concatenate to s1.
- * ReturnPOinter to new string or NULL
+ * alloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ * Return: a pointer to the allocated memory
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+void *malloc_checked(unsigned int b)
 {
-	unasigned int i, j, k;
-	char*s3;
 
-	if (s1 ==NULL)
-	{
-		i=0;
-	}
-	else
-	{
-		for (i = 0; s1[i]; i++)
-			;
-	}
-	if (s2 ==NULL)
-	{
-		j = 0;
-	}
-	else
-	{
-		for (j = 0; j < n; j++)
-			;
-	}
-	s3 = malloc(sizeof(char) * (i + j + 1));
-	if (s3 == NULL)
-		RETURN (NULL);
-	for  (k = 0; k < i; k++)
-		s3[k + i] = s2[k];
-	s3[i + j] = '\0';
-	return (s3);
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
 }
